1c91c315d019f4f8d12877753368e16114b15050
Sahar Bracha <saharulit@gmail.com>
2024-12-22 14:27:51 +0200

    fix

--- server/server.ts
+++ server/server.ts
@@ -3,7 +3,10 @@ import dotenv from 'dotenv';
 import cors from 'cors';
 
 import { authenticationRoute } from './src/routes/authenticationRoute';
+import { customerRoute } from './src/routes/customerRoute';
+
 import { connectMongoDB } from './connect';
+import { authenticate } from './src/auth.middleware';
 
 const app = express();
 const PORT = 3010;
@@ -14,7 +17,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api/auth', authenticationRoute);
-app.use('/api/customer', authenticationRoute);
+app.use('/api/customer', authenticate, customerRoute);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
